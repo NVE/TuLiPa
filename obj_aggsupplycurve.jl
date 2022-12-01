0c0fecc45321b8ac37c0966216723c4b5b24ac57
culi122 <julien.cabrol7@gmail.com>
2022-12-01 15:45:10 +0100

    Comment obj_aggsupplycurve

--- obj_aggsupplycurve.jl
+++ obj_aggsupplycurve.jl
@@ -1,3 +1,25 @@
+"""
+We implement BaseAggSupplyCurve
+
+This type takes a list of "simple" Flows that are connected to the same balance 
+("simple" as they are only connected to one balance).
+The parameter numclusters is the number of equivalent flows (or variables) that
+should represent the list of Flows
+This object only considers the cost, upper capacity and lower capacity
+when aggregating the flows to equivalent flows
+The aggregation is done with kmeans clustering of the costs
+The equivalent flows is represented with the mean cost, and the sum
+of lower and upper capacities of each cluster
+
+NB! Other traits (like startupcosts and softbounds) are not considered
+in the clustering and should be deleted if this type is used
+
+This method assumes that the Flows are connected to an endogenous Balance.
+If the Balance is exogenous the "simple" Flows are excessive
+
+# TODO: SegmentedArrow (efficiency segments) not supported. Add support or add check. 
+"""
+
 struct BaseAggSupplyCurve <: AggSupplyCurve
     id::Id
     balance::Balance
@@ -12,6 +34,7 @@ getnumclusters(var::BaseAggSupplyCurve) = var.numclusters
 
 getmainmodelobject(var::AggSupplyCurve) = var.balance
 
+# Build the equivalent variables
 function build!(p::Prob, var::AggSupplyCurve)
     varname = getinstancename(var.id)
     eqperiods = getnumperiods(gethorizon(getcommodity(var.balance)))
@@ -22,6 +45,7 @@ function build!(p::Prob, var::AggSupplyCurve)
     end  
 end
 
+# Include the equivalent Flows in the endogenous Balance
 function setconstants!(p::Prob, var::AggSupplyCurve)
     varname = getinstancename(var.id)
     eqinstance = split(varname, "PlantAgg_")[2]
@@ -35,7 +59,9 @@ function setconstants!(p::Prob, var::AggSupplyCurve)
     end
 end
 
+# Update the problem with cost and upper/lower bound for each equivalent flow
 function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
+    # Initialize
     horizon = gethorizon(getcommodity(var.balance))
     T = getnumperiods(horizon)
     
@@ -56,8 +82,11 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
         querydeltas[t] = gettimedelta(horizon, t)
     end
 
+    # Calculate costs and upper/lower bound for each flow and timeperiod
     for i in 1:numflows
         flow = var.flows[i]
+
+        # Cost
         cost = getcost(flow)
         if isconstant(cost)
             paramvalue = getparamvalue(cost, dummytime, dummydelta)
@@ -69,6 +98,8 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
                 mcs[t,i] = getparamvalue(cost, querystarts[t], querydeltas[t]) # pq not supported
             end
         end   
+
+        # Lower bound
         lb = getlb(flow)
         if isconstant(lb) && !isdurational(lb)
             paramvalue = getparamvalue(lb, dummytime, dummydelta)
@@ -80,6 +111,8 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
                 lbs[t,i] = getparamvalue(lb, querystarts[t], querydeltas[t])
             end
         end
+
+        # Upper bound
         ub = getub(flow)
         if isconstant(ub) && !isdurational(ub)
             paramvalue = getparamvalue(ub, dummytime, dummydelta)
@@ -93,11 +126,15 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
         end
     end
     
+    # Constant seed for reproducability of clustering
     Random.seed!(1000)
+
+    # For each timeperiod cluster flows by costs
     for t in 1:T
         r = kmeans(reshape(mcs[t,:],1,numflows), var.numclusters)
         assignments = r.assignments
 
+        # For each cluster aggregate costs and upper/lower bounds
         for assignment in 1:var.numclusters
             mc = 0
             lb = 0
@@ -110,7 +147,7 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
                 end
             end
 
-            if ub == 0
+            if ub == 0 # TODO: More robust. Checks
                 mc = 100000
             else
                 mc /= ub
@@ -119,6 +156,7 @@ function update!(p::Prob, var::AggSupplyCurve, start::ProbTime)
             newname = string(varname,"_",assignment)
             newid = Id(AGGSUPPLYCURVE_CONCEPT, newname)
 
+            # Set costs and upper/lower bounds for timeperiod and cluster
             setobjcoeff!(p, newid, t, float(mc))
             setlb!(p, newid, t, float(lb))
             setub!(p, newid, t, float(ub))
