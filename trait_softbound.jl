c304fd542c56bf561900a8fad206a55a3b9ad43f
culi122 <julien.cabrol7@gmail.com>
2022-12-06 13:08:44 +0100

    Comment softbound

--- trait_softbound.jl
+++ trait_softbound.jl
@@ -1,3 +1,11 @@
+"""
+We implement BaseSoftBound (see abstracttypes.jl)
+
+It builds the following equation depending on if the the 
+softbound is upper (sign = 1) or lower (sign = -1):
+sign*var[t] - breachvar[t] <= sign*softcap
+"""
+
 # ----------- Concrete types -------------
 mutable struct BaseSoftBound <: SoftBound
     id::Id
@@ -5,10 +13,6 @@ mutable struct BaseSoftBound <: SoftBound
     softcap::Param
     penalty::Param
     isupper::Bool
-    
-    function BaseSoftBound(id, var, softcap, penalty, isupper)
-        return new(id, var, softcap, penalty, isupper)
-    end
 end
 
 # --------- Interface functions ------------
@@ -19,6 +23,129 @@ getpenalty(trait::BaseSoftBound) = trait.penalty
 isupper(trait::BaseSoftBound) = trait.isupper
 
 getmainmodelobject(trait::BaseSoftBound) = trait.var
+
+# BaseSoftBound creates variables and equations that needs ids/names
+function getleid(trait::BaseSoftBound)
+    Id(getconceptname(trait.id), "Le" * getinstancename(trait.id))
+end
+
+function getbreachvarid(trait::BaseSoftBound)
+    Id(getconceptname(trait.id), "Breach" * getinstancename(trait.id))
+end
+    
+function getsoftcapid(trait::BaseSoftBound)
+    Id(getconceptname(trait.id), "SoftCap" * getinstancename(trait.id))
+end
+
+# ---------- build!, setconstants! and update! ---------
+
+# Build variables and equations for softbound equation
+function build!(p::Prob, trait::BaseSoftBound)
+    T = getnumperiods(gethorizon(getvar(trait)))
+
+    addvar!(p, getbreachvarid(trait), T)
+    addle!(p, getleid(trait), T)
+
+    return
+end
+
+function setconstants!(p::Prob, trait::BaseSoftBound)
+    horizon = gethorizon(trait.var)
+    T = getnumperiods(horizon)
+
+    # Positive or negative contribution in softbound equation
+    # depending on upper or lower softbound
+    if trait.isupper
+        sign = 1.0 # sign used for rhs and var in equation
+    else
+        sign = -1.0
+    end
+        
+    varid = getid(trait.var)
+    leid = getleid(trait)
+    breachvarid = getbreachvarid(trait)
+    softcapid = getsoftcapid(trait)
+
+    for t in 1:T
+        # Non-negative breachvars
+        setlb!(p, breachvarid, t, 0.0)
+
+        # Var and breachvar in softbound equation
+        setconcoeff!(p, leid, varid, t, t, sign)
+        setconcoeff!(p, leid, breachvarid, t, t, -1.0)
+    end
+
+    # Set breach penalty in objective function if its not constant
+    if isconstant(trait.penalty)
+        c = getparamvalue(trait.penalty, ConstantTime(), MsTimeDelta(Hour(1)))
+
+        for t in 1:T
+            setobjcoeff!(p, breachvarid, t, c)
+        end
+    end
+    
+    # Set RHS in softbound if it is the same for all scenarios and horizon periods
+    if !_must_dynamic_update(trait.softcap, horizon)
+        if isdurational(trait.softcap)
+            for t in 1:T
+                querydelta = gettimedelta(horizon, t)
+                value = getparamvalue(trait.softcap, ConstantTime(), querydelta)
+                setrhsterm!(p, leid, softcapid, t, value*sign)
+            end
+        else
+            value = getparamvalue(trait.softcap, ConstantTime(), MsTimeDelta(Hour(1)))
+            for t in 1:T
+                setrhsterm!(p, leid, softcapid, t, value*sign)
+            end
+        end
+    end
+
+    return
+end
+
+# See comments in setconstants!
+function update!(p::Prob, trait::BaseSoftBound, start::ProbTime)
+    horizon = gethorizon(trait.var)
+    T = getnumperiods(horizon)
+    
+    if trait.isupper
+        sign = 1.0
+    else
+        sign = -1.0
+    end
+    
+    if !isconstant(trait.penalty)
+        softcapid = getsoftcapid(trait)
+        for t in 1:T
+            querystart = getstarttime(horizon, t, start)
+            querydelta = gettimedelta(horizon, t)
+            c = getparamvalue(trait.penalty, querystart, querydelta)
+            
+            setobjcoeff!(p, breachvarid, t, c)
+        end
+    end
+    
+    if _must_dynamic_update(trait.softcap, horizon)
+        softcapid = getsoftcapid(trait)
+        leid = getleid(trait)
+        for t in 1:T
+            querystart = getstarttime(horizon, t, start)
+            querydelta = gettimedelta(horizon, t)
+            value = getparamvalue(trait.softcap, querystart, querydelta)
+            
+            setrhsterm!(p, leid, softcapid, t, value*sign)
+        end
+    end
+    return
+end
+
+# BaseSoftBound types are toplevel objects in dataset_compiler, som we must implement assemble!
+function assemble!(trait::BaseSoftBound)
+    # return if var not assembled yet
+    isnothing(gethorizon(trait.var)) && return false
+
+    return true
+end
 
 # ------ Include dataelements -------
 function includeBaseSoftBound!(toplevel::Dict, lowlevel::Dict, elkey::ElementKey, value::Dict)::Bool
@@ -45,116 +172,3 @@ end
 
 INCLUDEELEMENT[TypeKey(SOFTBOUND_CONCEPT, "BaseSoftBound")] = includeBaseSoftBound!
 
-function assemble!(trait::BaseSoftBound)
-    # return if var not assembled yet
-    isnothing(gethorizon(trait.var)) && return false
-
-    return true
-end
-
-function getleid(trait::BaseSoftBound)
-    Id(getconceptname(trait.id), "Le" * getinstancename(trait.id))
-end
-
-function getbreachvarid(trait::BaseSoftBound)
-    Id(getconceptname(trait.id), "Breach" * getinstancename(trait.id))
-end
-    
-function getsoftcapid(trait::BaseSoftBound)
-    Id(getconceptname(trait.id), "SoftCap" * getinstancename(trait.id))
-end
-
-function build!(p::Prob, trait::BaseSoftBound)
-    T = getnumperiods(gethorizon(getvar(trait)))
-
-    addvar!(p, getbreachvarid(trait), T)
-    addle!(p, getleid(trait), T)
-
-    return
-end
-
-function setconstants!(p::Prob, trait::BaseSoftBound)
-    horizon = gethorizon(trait.var)
-    T = getnumperiods(horizon)
-
-    if trait.isupper
-        rhssign = 1.0 # same as vardirection
-    else
-        rhssign = -1.0
-    end
-        
-    varid = getid(trait.var)
-    leid = getleid(trait)
-    breachvarid = getbreachvarid(trait)
-    softcapid = getsoftcapid(trait)
-
-    if isconstant(trait.penalty)
-        c = getparamvalue(trait.penalty, ConstantTime(), MsTimeDelta(Hour(1)))
-
-        for t in 1:T
-            setobjcoeff!(p, breachvarid, t, c)
-        end
-    end
-    
-    if !_must_dynamic_update(trait.softcap, horizon)
-        if isdurational(trait.softcap)
-            for t in 1:T
-                querydelta = gettimedelta(horizon, t)
-                value = getparamvalue(trait.softcap, ConstantTime(), querydelta)
-                setrhsterm!(p, leid, softcapid, t, value*rhssign)
-            end
-        else
-            value = getparamvalue(trait.softcap, ConstantTime(), MsTimeDelta(Hour(1)))
-            for t in 1:T
-                setrhsterm!(p, leid, softcapid, t, value*rhssign)
-            end
-        end
-    end
-
-    for t in 1:T
-        # Non-negative vars
-        setlb!(p, breachvarid, t, 0.0)
-
-        # rhssign*boundedvar[t] - breachvar[t] <= rhssign*softcap
-        setconcoeff!(p, leid, varid, t, t, rhssign)
-        setconcoeff!(p, leid, breachvarid, t, t, -1.0)
-    end
-    return
-end
-
-function update!(p::Prob, trait::BaseSoftBound, start::ProbTime)
-    horizon = gethorizon(trait.var)
-    T = getnumperiods(horizon)
-    
-    if trait.isupper
-        rhssign = 1.0 # same as vardirection
-    else
-        rhssign = -1.0
-    end
-    
-    # Set varective coeff if its not constant
-    if !isconstant(trait.penalty)
-        softcapid = getsoftcapid(trait)
-        for t in 1:T
-            querystart = getstarttime(horizon, t, start)
-            querydelta = gettimedelta(horizon, t)
-            c = getparamvalue(trait.penalty, querystart, querydelta)
-            
-            setobjcoeff!(p, breachvarid, t, c)
-        end
-    end
-    
-    # Set rhs if its not constant
-    if _must_dynamic_update(trait.softcap, horizon)
-        softcapid = getsoftcapid(trait)
-        leid = getleid(trait)
-        for t in 1:T
-            querystart = getstarttime(horizon, t, start)
-            querydelta = gettimedelta(horizon, t)
-            value = getparamvalue(trait.softcap, querystart, querydelta)
-            
-            setrhsterm!(p, leid, softcapid, t, value*rhssign)
-        end
-    end
-    return
-end
