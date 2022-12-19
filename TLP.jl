34574b42221a0edf16b836b48a1e37dd4278f27e
culi122 <julien.cabrol7@gmail.com>
2022-12-19 15:46:56 +0100

    Comment TLP

--- TLP.jl
+++ TLP.jl
@@ -1,43 +1,51 @@
+# Abstract types in our modelling framework and general descriptions
 include("abstracttypes.jl")
 
-include("utils_datetime.jl")
-
-include("data_types.jl")
+# Code to convert data elements into model objects
+include("data_elements.jl")
 include("data_elements_to_objects.jl")
 include("data_constants.jl")
 include("data_utils.jl")
 
-include("times.jl")
-include("timedeltas.jl")
-include("timevectors.jl")
-include("timeperiods.jl")
-include("horizons.jl")
-
+# Problem implementation around JuMP framework and HiGHS package
 include("problem_jump.jl")
 include("problem_highs.jl")
 
+# Time, time-series and horizons
+include("utils_datetime.jl") # functions for datetime
+include("times.jl") # problem times
+include("timevectors.jl") # time-series data
+include("timeperiods.jl") # describe simulation/scenario start and stop
+include("timedeltas.jl")
+include("horizons.jl")
+
+# Lowlevel model objects
+# see data_elements_to_objects.jl for description of difference between lowlevel and toplevel
 include("trait_conversion.jl")
 include("trait_loss.jl")
 include("trait_cost.jl")
 include("trait_price.jl")
 include("trait_capacity.jl")
-
-include("parameters.jl")
-
-include("state_variables.jl")
-include("boundary_conditions.jl")
-
 include("trait_arrow.jl")
 include("trait_rhsterm.jl")
+include("trait_commodity.jl")
+include("trait_metadata.jl")
 
+# Parameters for model objects and traits (Lowlevel)
+include("parameters.jl")
+
+# Toplevel model objects
 include("obj_balance.jl")
 include("obj_flow.jl")
 include("obj_storage.jl")
 include("obj_aggsupplycurve.jl")
-
-include("trait_commodity.jl")
-include("trait_metadata.jl")
 include("trait_softbound.jl")
 include("trait_startupcost.jl")
 
+# State variables and boundary conditions
+include("state_variables.jl")
+include("boundary_conditions.jl") # (Toplevel)
+
+# Code to manipulate model objects
+# (e.g. alter, aggregate, distinguish features)
 include("reasoning_modelobjects.jl")
