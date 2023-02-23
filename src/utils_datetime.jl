f975c33efab8c9d4f6be779f2d4f9bc08bb4bd5e
culi122 <40186418+cjuli1@users.noreply.github.com>
2023-02-23 09:42:31 +0100

    Improve performance utils_datetime

--- src/utils_datetime.jl
+++ src/utils_datetime.jl
@@ -9,7 +9,7 @@ isisoyearstart(t::DateTime) = (week(t) == 1) && (dayofweek(t) == 1) && (Time(t) 
 """
 Get datetime that is start of iso year
 """
-getisoyearstart(t::DateTime) = DateTime(Date(t + Day(8 - 7*week(t) - dayofweek(t))))
+getisoyearstart(t::DateTime) = t + Millisecond(3600000*24*(8 - 7*week(t) - dayofweek(t)) - Dates.value(t) % 86400000)
 getisoyearstart(isoyear::Int) = getisoyearstart(DateTime(isoyear, 5, 1))
 
 """
@@ -22,5 +22,5 @@ getisoyear(t::DateTime) = year(t + Week(15 - week(t)))
 Get datetime for isoyear that is similar iso time of year as input datetime t
 """
 function getsimilardatetime(t::DateTime, isoyear::Int)
-    DateTime(Date(getisoyearstart(isoyear) + Day(7*week(t) + dayofweek(t) - 8)), Time(t))
+    getisoyearstart(isoyear) + Millisecond(3600000*24*(7*week(t) + dayofweek(t) - 8) + Dates.value(t) % 86400000)
 end
