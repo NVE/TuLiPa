9327fad58fd95afaa50cb7f2bcd2faa9460a5ab8
culi122 <40186418+cjuli1@users.noreply.github.com>
2023-10-12 20:05:10 +0200

    Remove include BaseHorizon

--- src/horizons.jl
+++ src/horizons.jl
@@ -690,24 +690,7 @@ function assign_blocks!(method::KMeansAHMethod, X::Vector{Float64})
 end
 
 # ------ Include dataelements -------
-function includeBaseHorizon!(::Dict, lowlevel::Dict, elkey::ElementKey, value::Dict)::Bool
-    checkkey(lowlevel, elkey)
-    
-    numperiods = getdictvalue(value, "NumPeriods",      Int,                      elkey)
-    timedelta  = getdictvalue(value, TIMEDELTA_CONCEPT, Union{String, TimeDelta}, elkey)
-    
-    if timedelta isa String
-        timedeltakey = BaseId(TIMEDELTA_CONCEPT, timedelta)
-        haskey(lowlevel, timedeltakey) || return false
-        timedelta = lowlevel[timedelta]
-    end
-    
-    lowlevel[getobjkey(elkey)] = BaseHorizon(numperiods, timedelta)
-    return true
-end
-
-INCLUDEELEMENT[TypeKey(HORIZON_CONCEPT, "BaseHorizon")] = includeBaseHorizon!
-
+# TODO
 
 
 
