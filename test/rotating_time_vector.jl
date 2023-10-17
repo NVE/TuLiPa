384487dd30589464ae587490d54d324fc9c06716
pertft <per.thorbjornsen@bouvet.no>
2023-10-17 14:25:31 +0200

    more detailed rotating timevector test

--- test/rotating_time_vector.jl
+++ test/rotating_time_vector.jl
@@ -1,28 +1,47 @@
 using Dates, TuLiPa
 
 # RotationTimeVector only rotate after a full year, not based on start stop dates.
-obj_test = PositiveCapacity(Id("Capacity", "Capacity_test"),
-        UMMSeriesParam(
-                InfiniteTimeVector(
-                        [DateTime("2023-01-01T00:00:00"),
-                                DateTime("2023-01-02T00:00:00"),
-                                DateTime("2023-01-03T00:00:00")],
-                        [1, 1, 1]),  # level
-                InfiniteTimeVector(
-                        [DateTime("2023-01-01T00:00:00")],
-                        [1]), # umm profile
-                RotatingTimeVector(
-                        [DateTime("1980-01-01T00:00:00"),
-                                DateTime("1980-01-02T00:00:00"),
-                                DateTime("1980-01-03T00:00:00")],
-                        [0, 10, 0],  # profile
-                        DateTime("1980-01-01T00:00:00"), #start
-                        DateTime("1980-01-03T00:00:00")) #stop 
-        ), true)
+obj_test = 
+        RotatingTimeVector(
+            [DateTime("1980-01-01T00:00:00"), 
+            DateTime("1980-01-02T00:00:00"),
+            DateTime("1980-01-03T00:00:00")], 
+            [0, 10, 5], 
+            DateTime("1980-01-01T00:00:00"), #start
+            DateTime("1980-01-04T00:00:00")) #stop 
 
 pt = TwoTime(DateTime("2023-01-01T00:00:00"), DateTime("1980-01-01T00:00:00"))
-horizon = SequentialHorizon(700, Day(1))
+horizon = SequentialHorizon(700, Day(1)) 
+
+t = 1
+querystart = getstarttime(horizon, t, pt)
+querydelta = gettimedelta(horizon, t)
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 0
+
+t = 2
+querystart = getstarttime(horizon, t, pt)
+querydelta = gettimedelta(horizon, t)
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 10
+
+t = 3
+querystart = getstarttime(horizon, t, pt)
+querydelta = gettimedelta(horizon, t)
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 5
+
+# Same period as the stop value ("1980-01-04T00:00:00") gives 0.
+t = 4
+querystart = getstarttime(horizon, t, pt)
+querydelta = gettimedelta(horizon, t)
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 0
+
+# After that repeats last value in the array.
+t = 5
+querystart = getstarttime(horizon, t, pt)
+querydelta = gettimedelta(horizon, t)
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 5
+
+# After a year it will rotate.
 t = 366
 querystart = getstarttime(horizon, t, pt)
 querydelta = gettimedelta(horizon, t)
-getparamvalue(obj_test.param, querystart, querydelta) == 0.24
+@test getweightedaverage(obj_test, querystart.scenariotime, querydelta) == 10
