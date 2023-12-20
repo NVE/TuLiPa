418a9a682baeb9af014dd4f0bbfd6d39cbc6018c
culi122 <40186418+cjuli1@users.noreply.github.com>
2023-12-20 10:23:27 +0100

    move update! to common

--- src/problem_common.jl
+++ src/problem_common.jl
@@ -3,4 +3,21 @@ Contains functions that are shared among all the problem types
 (problem_cplex, problem_highs, problem_jump)
 """
 
-is_CPLEX_Prob(p::Prob) = false # Used to check if the problem type is cplex without having the cplex package
+# -------- Update problem for given problem time --------------
+function update!(p::Prob, start::ProbTime)
+    # Update horizons that need to dynamically update
+    for horizon in gethorizons(p)
+        update!(horizon, start)
+    end
+    
+    # Loop through all model objects. Set parameters and coefficients that
+    # depend on the problem time and period in the horizon.
+    # The generic function update! has different methods depending on the inputed object
+    # Some objects will again call update! on its internal traits
+    for obj in getobjects(p)
+        update!(p, obj, start)
+    end
+end
+
+# ----- Check if specific problem type ----------
+is_CPLEX_Prob(p::Prob) = false
