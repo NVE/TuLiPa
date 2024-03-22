93998d8e5a275e26b19a5fed352e9f07485c3b07
Harald <haen@nve.no>
2024-03-22 12:34:40 +0100

    removed validation.jl in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,5 @@ include("timeoffset.jl")
 include("highs_equals_jump_results.jl")
 # include("rotating_time_vector.jl")  # TODO: Modify tests to handle one year data
 include("umm.jl")
-include("validation.jl")
 include("prognosis_series_param.jl")
 include("apply_settings.jl")
