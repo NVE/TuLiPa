1364ffce92171c0417ef86b27ae724631d734d62
Harald <haen@nve.no>
2024-03-25 12:27:39 +0100

    added empty includeelement func tests

--- test/includeelement_functions.jl
+++ test/includeelement_functions.jl
@@ -39,6 +39,8 @@ overwrite names the global namespace, which could affect other
 tests in when running the runtests.jl script.
 """
 
+# TODO: Complete empty tests 
+
 module Test_INCLUDEELEMENT_Methods
 
 using TuLiPa, Test, Dates
@@ -61,7 +63,6 @@ end
 function test_is_all_includeelement_methods_covered()
     ACTUAL_INCLUDE_METHODS = Tuple{Function, Int}[]
     for f in values(INCLUDEELEMENT)
-        f === includeVectorTimeIndex! || f === includeRangeTimeIndex! || continue    # TODO: Remove later
         push!(ACTUAL_INCLUDE_METHODS, (f, length(methods(f))))
     end
     
@@ -87,6 +88,55 @@ end
 function test_all_includeelement_methods()
     test_includeVectorTimeIndex!()
     test_includeRangeTimeIndex!()
+    test_includeVectorTimeValues!()
+    test_includeBaseTable!()
+    test_includeColumnTimeValues!()
+    test_includeRotatingTimeVector!()
+    test_includeOneYearTimeVector!()
+    test_includeInfiniteTimeVector!()
+    test_includeMutableInfiniteTimeVector!()
+    test_includeConstantTimeVector!()
+    test_includeStartEqualStop!()
+    test_includeBaseBalance!()
+    test_includeExogenBalance!()
+    test_includeBaseFlow!()
+    test_includeBaseStorage!()
+    test_includeFossilMCParam!()
+    test_includeM3SToMM3Param!()
+    test_includeM3SToMM3SeriesParam!()
+    test_includeMWToGWhSeriesParam!()
+    test_includeMWToGWhParam!()
+    test_includeCostPerMWToGWhParam!()
+    test_includeMeanSeriesParam!()
+    test_includeMeanSeriesIgnorePhaseinParam!()
+    test_includePrognosisSeriesParam!()
+    test_includeMeanSeriesIgnorePhaseinParam!()
+    test_includeStatefulParam!()
+    test_includeMsTimeDelta!()
+    test_includeScenarioTimePeriod!()
+    test_includeSimulationTimePeriod!()
+    test_includeBaseArrow!()
+    test_includeSegmentedArrow!()
+    test_includePositiveCapacity!()
+    test_includeLowerZeroCapacity!()
+    test_includeBaseCommodity!()
+    test_includeBaseConversion!()
+    test_includePumpConversion!()
+    test_includeCostTerm!()
+    test_includeSimpleLoss!()
+    test_includeStoragehint!()
+    test_includeResidualhint!()
+    test_includeReservoirCurve!()
+    test_includeProductionInfo!()
+    test_includeHydraulichint!()
+    test_includeGlobalEneq!()
+    test_includeBasePrice!()
+    test_includeTransmissionRamping!()
+    test_includeHydroRampingWithout!()
+    test_includeHydroRamping!()
+    test_includeBaseRHSTerm!()
+    test_includeBaseSoftBound!()
+    test_includeSimpleStartUpCost!()
 end
 
 function test_includeVectorTimeIndex!()
@@ -173,10 +223,10 @@ function test_includeRangeTimeIndex!()
     # same id already stored in lowlevel error
     LL[Id(k.conceptname, k.instancename)] = 1
     @test_throws ErrorException includeRangeTimeIndex!(TL, LL, k, d)
-    # negative Millisecond error
+    # non-positive Millisecond error
     (TL, LL) = (Dict(), Dict())
     t = DateTime(1985, 7, 1)
-    d = Millisecond(Hour(-1))
+    d = Millisecond(Hour(0))
     r = StepRange(t, d, t)
     @test_throws ErrorException includeRangeTimeIndex!(TL, LL, k, r)
     # should be ok
@@ -187,7 +237,263 @@ function test_includeRangeTimeIndex!()
     ret = includeRangeTimeIndex!(TL, LL, k, r)
     _test_ret(ret)
 
+    # TODO: also test that t0 > t1 in tincludeRangeTimeIndex! for value::StepRange?
+
     register_tested_methods(includeRangeTimeIndex!, 2)
+end
+
+function test_includeVectorTimeValues!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeVectorTimeValues!, 1)
+end
+
+function test_includeBaseTable!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseTable!, 1)
+end
+
+function test_includeColumnTimeValues!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeColumnTimeValues!, 1)
+end
+
+function test_includeRotatingTimeVector!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeRotatingTimeVector!, 1)
+end
+
+function test_includeOneYearTimeVector!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeOneYearTimeVector!, 1)
+end
+
+function test_includeInfiniteTimeVector!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeInfiniteTimeVector!, 1)
+end
+
+function test_includeMutableInfiniteTimeVector!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeMutableInfiniteTimeVector!, 1)
+end
+
+function test_includeConstantTimeVector!()
+  # TODO: test for value::Dict
+  # TODO: test for value::AbstractFloat
+  # TODO: test for value::ConstantTimeVector
+  register_tested_methods(includeConstantTimeVector!, 3)
+end
+
+function test_includeStartEqualStop!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeStartEqualStop!, 1)
+end
+
+function test_includeBaseBalance!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseBalance!, 1)
+end
+
+function test_includeExogenBalance!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeExogenBalance!, 1)
+end
+
+function test_includeBaseFlow!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseFlow!, 1)
+end
+
+function test_includeBaseStorage!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseStorage!, 1)
+end
+
+function test_includeFossilMCParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeFossilMCParam!, 1)
+end
+
+function test_includeM3SToMM3Param!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeM3SToMM3Param!, 1)
+end
+
+function test_includeM3SToMM3SeriesParam!()
+  # TODO: test for value::Dict
+  # TODO: test for value::M3SToMM3SeriesParam
+  # TODO: test for value::AbstractFloat
+  register_tested_methods(includeM3SToMM3SeriesParam!, 3)
+end
+
+function test_includeMWToGWhSeriesParam!()
+  # TODO: test for value::Dict
+  # TODO: test for value::MWToGWhSeriesParam
+  # TODO: test for value::AbstractFloat
+  register_tested_methods(includeMWToGWhSeriesParam!, 3)
+end
+
+function test_includeMWToGWhParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeMWToGWhParam!, 1)
+end
+
+function test_includeCostPerMWToGWhParam!()
+  # TODO: test for value::Dict
+  # TODO: test for value::CostPerMWToGWhParam
+  # TODO: test for value::AbstractFloat
+  register_tested_methods(includeCostPerMWToGWhParam!, 3)
+end
+
+function test_includeMeanSeriesParam!()
+  # TODO: test for value::Dict
+  # TODO: test for value::MeanSeriesParam
+  register_tested_methods(includeMeanSeriesParam!, 2)
+end
+
+function test_includeMeanSeriesIgnorePhaseinParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeMeanSeriesIgnorePhaseinParam!, 1)
+end
+
+function test_includePrognosisSeriesParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includePrognosisSeriesParam!, 1)
+end
+
+function test_includeMeanSeriesIgnorePhaseinParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeMeanSeriesIgnorePhaseinParam!, 1)
+end
+
+function test_includeStatefulParam!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeStatefulParam!, 1)
+end
+
+function test_includeMsTimeDelta!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeMsTimeDelta!, 1)
+end
+
+function test_includeScenarioTimePeriod!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeScenarioTimePeriod!, 1)
+end
+
+function test_includeSimulationTimePeriod!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeSimulationTimePeriod!, 1)
+end
+
+function test_includeBaseArrow!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseArrow!, 1)
+end
+
+function test_includeSegmentedArrow!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeSegmentedArrow!, 1)
+end
+
+function test_includePositiveCapacity!()
+  # TODO: test for value::Dict
+  register_tested_methods(includePositiveCapacity!, 1)
+end
+
+function test_includeLowerZeroCapacity!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeLowerZeroCapacity!, 1)
+end
+
+function test_includeBaseCommodity!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseCommodity!, 1)
+end
+
+function test_includeBaseConversion!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseConversion!, 1)
+end
+
+function test_includePumpConversion!()
+  # TODO: test for value::Dict
+  register_tested_methods(includePumpConversion!, 1)
+end
+
+function test_includeCostTerm!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeCostTerm!, 1)
+end
+
+function test_includeSimpleLoss!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeSimpleLoss!, 1)
+end
+
+function test_includeStoragehint!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeStoragehint!, 1)
+end
+
+function test_includeResidualhint!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeResidualhint!, 1)
+end
+
+function test_includeReservoirCurve!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeReservoirCurve!, 1)
+end
+
+function test_includeProductionInfo!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeProductionInfo!, 1)
+end
+
+function test_includeHydraulichint!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeHydraulichint!, 1)
+end
+
+function test_includeGlobalEneq!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeGlobalEneq!, 1)
+end
+
+function test_includeBasePrice!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBasePrice!, 1)
+end
+
+function test_includeTransmissionRamping!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeTransmissionRamping!, 1)
+end
+
+function test_includeHydroRampingWithout!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeHydroRampingWithout!, 1)
+end
+
+function test_includeHydroRamping!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeHydroRamping!, 1)
+end
+
+function test_includeBaseRHSTerm!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseRHSTerm!, 1)
+end
+
+function test_includeBaseSoftBound!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeBaseSoftBound!, 1)
+end
+
+function test_includeSimpleStartUpCost!()
+  # TODO: test for value::Dict
+  register_tested_methods(includeSimpleStartUpCost!, 1)
 end
 
 function _test_ret(ret; n=0, okvalue=true, depstype=Vector{Id})
