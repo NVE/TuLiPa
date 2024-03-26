deee046ea2b067564e3de5fa7d15fe952eae16d7
Harald <haen@nve.no>
2024-03-26 10:46:44 +0100

    module and @testset

--- test/prognosis_series_param.jl
+++ test/prognosis_series_param.jl
@@ -1,14 +1,24 @@
-using TuLiPa, Dates
+module TestPrognosisSeriesParam
 
-v = InfiniteTimeVector{Vector{DateTime},Vector{Float64}}(
-        [DateTime("2023-01-01T00:00:00"),
-         DateTime("2023-01-02T00:00:00"),
-         DateTime("2023-01-03T00:00:00")],
-        [10, 10, 10])
+using TuLiPa, Dates, Test
 
-horizon = SequentialHorizon(40, Day(1))
-obj_test = PositiveCapacity(Id("Capacity", "Capacity_test"), PrognosisSeriesParam(v, v, v, 1), true)
-pt = PrognosisTime(DateTime("2023-01-01T01:00:00"), DateTime("2023-01-01T01:00:00"), DateTime("2023-01-01T01:00:00"))
-querystart = getstarttime(horizon, 1, pt)
-querydelta = gettimedelta(horizon, 1)
-@test getparamvalue(obj_test.param, querystart, querydelta) == 100
+function run_tests()
+    v = InfiniteTimeVector{Vector{DateTime},Vector{Float64}}(
+            [DateTime("2023-01-01T00:00:00"),
+            DateTime("2023-01-02T00:00:00"),
+            DateTime("2023-01-03T00:00:00")],
+            [10, 10, 10])
+
+    horizon = SequentialHorizon(40, Day(1))
+    obj_test = PositiveCapacity(Id("Capacity", "Capacity_test"), PrognosisSeriesParam(v, v, v, 1), true)
+    pt = PrognosisTime(DateTime("2023-01-01T01:00:00"), DateTime("2023-01-01T01:00:00"), DateTime("2023-01-01T01:00:00"))
+    querystart = getstarttime(horizon, 1, pt)
+    querydelta = gettimedelta(horizon, 1)
+    @test getparamvalue(obj_test.param, querystart, querydelta) == 100
+end
+
+@testset "PrognosisSeriesParam" begin
+    run_tests()
+end
+
+end
