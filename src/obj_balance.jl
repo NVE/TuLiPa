011c55dba5c6ceb85d054b92453c8a4ae88a4514
Harald <haen@nve.no>
2024-08-02 14:18:21 +0200

    Bugfix: use isstateful in update!(::Prob, ::BaseBalance, ::ProbTime)

--- src/obj_balance.jl
+++ src/obj_balance.jl
@@ -106,15 +106,8 @@ end
 function update!(p::Prob, balance::BaseBalance, start::ProbTime)
     for rhsterm in balance.rhsterms
         if !isconstant(rhsterm) || !hasconstantdurations(balance.horizon)
-            for t in 1:getnumperiods(balance.horizon)
-                (future_t, ok) = mayshiftfrom(balance.horizon, t)
-                if ok
-                    value = getrhsterm(p, balance.id, getid(rhsterm), future_t)
-                    setrhsterm!(p, balance.id, getid(rhsterm), t, value)
-                end
-            end
-            for t in 1:getnumperiods(balance.horizon)
-                if mustupdate(balance.horizon, t)
+            if isstateful(rhsterm)
+                for t in 1:getnumperiods(balance.horizon)
                     querystart = getstarttime(balance.horizon, t, start)
                     querydelta = gettimedelta(balance.horizon, t)
                     value = getparamvalue(rhsterm, querystart, querydelta)
@@ -122,6 +115,25 @@ function update!(p::Prob, balance::BaseBalance, start::ProbTime)
                         value = -value
                     end
                     setrhsterm!(p, balance.id, getid(rhsterm), t, value)
+                end
+            else
+                for t in 1:getnumperiods(balance.horizon)
+                    (future_t, ok) = mayshiftfrom(balance.horizon, t)
+                    if ok
+                        value = getrhsterm(p, balance.id, getid(rhsterm), future_t)
+                        setrhsterm!(p, balance.id, getid(rhsterm), t, value)
+                    end
+                end
+                for t in 1:getnumperiods(balance.horizon)
+                    if mustupdate(balance.horizon, t)
+                        querystart = getstarttime(balance.horizon, t, start)
+                        querydelta = gettimedelta(balance.horizon, t)
+                        value = getparamvalue(rhsterm, querystart, querydelta)
+                        if !isingoing(rhsterm)
+                            value = -value
+                        end
+                        setrhsterm!(p, balance.id, getid(rhsterm), t, value)
+                    end
                 end
             end
         end
